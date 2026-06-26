4da5f40fd50f27c4f6dc44d77bee64f6179b000a
Purdze <r.s.sutton@hotmail.co.uk>
2026-06-26 20:38:27 +0100

    Use world RNG for ender pearl throw pitch

--- steel-core/src/behavior/items/ender_pearl.rs
+++ steel-core/src/behavior/items/ender_pearl.rs
@@ -10,6 +10,7 @@ use glam::DVec3;
 use steel_macros::item_behavior;
 use steel_protocol::packets::game::SoundSource;
 use steel_registry::{sound_events, vanilla_entities};
+use steel_utils::random::Random as _;
 
 use crate::behavior::context::{InteractionResult, UseItemContext};
 use crate::behavior::item::ItemBehavior;
@@ -29,8 +30,7 @@ impl ItemBehavior for EnderPearlItem {
         let player = context.player;
         let world = context.world;
 
-        // Vanilla throw pitch: 0.4 / (random * 0.4 + 0.8).
-        let pitch = 0.4 / (rand::random::<f32>() * 0.4 + 0.8);
+        let pitch = 0.4 / (world.random().lock().next_f32() * 0.4 + 0.8);
         world.play_sound_at(
             &sound_events::ENTITY_ENDER_PEARL_THROW,
             SoundSource::Neutral,
